2014d23b3ce25946fd605a05f29a44e22861847a
Niall Ó Broin <niall@turbulentdynamics.com>
2020-12-06 16:17:43 +0400

    Initial Commit

---
+++ tdLBCpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  tdLBCpp
+//
+//  Created by Niall Ó Broin on 06/12/2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
