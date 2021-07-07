179a785403aacbf8781e71fd74776f4ad291c24b
Andrey Galysh <galysh.andrey@gmail.com>
2021-07-07 14:18:49 +0300

    Fix StreamingNieve to fill appropriate densities

--- tdlbcpp/src/StreamingNieve.hpp
+++ tdlbcpp/src/StreamingNieve.hpp
@@ -21,18 +21,20 @@ void ComputeUnit<T, QVecSize>::streamingNieve(){
         for (tNi j=1; j<=yg1; j++){
             for (tNi k=1; k<=zg1; k++){
                 //DST    =  SRC
-                Q[index(i, j, k)].q[ Q1] = Q[ dirnQ1(i,j,k)].q[ Q1];
-                Q[index(i, j, k)].q[ Q4] = Q[ dirnQ4(i,j,k)].q[ Q4];
-                Q[index(i, j, k)].q[ Q5] = Q[ dirnQ5(i,j,k)].q[ Q5];
+                // moving towards Q5, then Q3, then Q1
+                // when moving we should not overwrite old values
+                Q[index(i, j, k)].q[ Q2] = Q[ dirnQ1(i,j,k)].q[ Q2];
+                Q[index(i, j, k)].q[ Q4] = Q[ dirnQ3(i,j,k)].q[ Q4];
+                Q[index(i, j, k)].q[ Q6] = Q[ dirnQ5(i,j,k)].q[ Q6];
 
-                Q[index(i, j, k)].q[Q13] = Q[dirnQ13(i,j,k)].q[Q13];
-                Q[index(i, j, k)].q[ Q7] = Q[ dirnQ7(i,j,k)].q[ Q7];
+                Q[index(i, j, k)].q[Q14] = Q[dirnQ13(i,j,k)].q[Q14];
+                Q[index(i, j, k)].q[ Q8] = Q[ dirnQ7(i,j,k)].q[ Q8];
                 
-                Q[index(i, j, k)].q[Q15] = Q[dirnQ15(i,j,k)].q[Q15];
-                Q[index(i, j, k)].q[ Q9] = Q[ dirnQ9(i,j,k)].q[ Q9];
+                Q[index(i, j, k)].q[Q16] = Q[dirnQ15(i,j,k)].q[Q16];
+                Q[index(i, j, k)].q[Q10] = Q[ dirnQ9(i,j,k)].q[Q10];
                 
-                Q[index(i, j, k)].q[Q17] = Q[dirnQ17(i,j,k)].q[Q17];
-                Q[index(i, j, k)].q[Q11] = Q[dirnQ11(i,j,k)].q[Q11];
+                Q[index(i, j, k)].q[Q18] = Q[dirnQ17(i,j,k)].q[Q18];
+                Q[index(i, j, k)].q[Q12] = Q[dirnQ11(i,j,k)].q[Q12];
             }
         }
     }
@@ -43,18 +45,18 @@ void ComputeUnit<T, QVecSize>::streamingNieve(){
             for (tNi k=zg1;  k>=1; k--) {
                 
                 //DST   =   SRC
-                Q[index(i, j, k)].q[ Q2] = Q[ dirnQ2(i,j,k)].q[ Q2];
-                Q[index(i, j, k)].q[ Q3] = Q[ dirnQ3(i,j,k)].q[ Q3];
-                Q[index(i, j, k)].q[ Q6] = Q[ dirnQ6(i,j,k)].q[ Q6];
+                Q[index(i, j, k)].q[ Q1] = Q[ dirnQ2(i,j,k)].q[ Q1];
+                Q[index(i, j, k)].q[ Q3] = Q[ dirnQ4(i,j,k)].q[ Q3];
+                Q[index(i, j, k)].q[ Q5] = Q[ dirnQ6(i,j,k)].q[ Q5];
 
-                Q[index(i, j, k)].q[ Q8] = Q[ dirnQ8(i,j,k)].q[ Q8];
-                Q[index(i, j, k)].q[Q14] = Q[dirnQ14(i,j,k)].q[Q14];
+                Q[index(i, j, k)].q[ Q7] = Q[ dirnQ8(i,j,k)].q[ Q7];
+                Q[index(i, j, k)].q[Q13] = Q[dirnQ14(i,j,k)].q[Q13];
                 
-                Q[index(i, j, k)].q[Q10] = Q[dirnQ10(i,j,k)].q[Q10];
-                Q[index(i, j, k)].q[Q16] = Q[dirnQ16(i,j,k)].q[Q16];
+                Q[index(i, j, k)].q[ Q9] = Q[dirnQ10(i,j,k)].q[ Q9];
+                Q[index(i, j, k)].q[Q15] = Q[dirnQ16(i,j,k)].q[Q15];
                 
-                Q[index(i, j, k)].q[Q12] = Q[dirnQ12(i,j,k)].q[Q12];
-                Q[index(i, j, k)].q[Q18] = Q[dirnQ18(i,j,k)].q[Q18];
+                Q[index(i, j, k)].q[Q11] = Q[dirnQ12(i,j,k)].q[Q11];
+                Q[index(i, j, k)].q[Q17] = Q[dirnQ18(i,j,k)].q[Q17];
             }
         }
     }
